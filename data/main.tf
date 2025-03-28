bfd3f8354e0f200da1d4322de57b480f519ca40e
Abdelmoumene Haouari <abdelhaouari22@gmail.com>
2025-03-28 14:43:09 -0400

    modifs .tf

--- data/main.tf
+++ data/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -44,11 +44,11 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-1234567"
+  ami           = "ami-12345678"
   instance_type = "t2.micro"
 
   provisioner "local-exec" {
-    command = "echo Hello, World"
+    command = "echo Hello World"
   }
 
   lifecycle {
