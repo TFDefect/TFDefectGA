7cbcb0d5ee1f0e1a571f98fe211c9cccc34b63de
Abdelmoumene Haouari <abdelhaouari22@gmail.com>
2025-04-14 22:17:55 -0400

    modification d'un bloc Terraform dans module.tf

--- data/module.tf
+++ data/module.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  version = "3.15.0"
 
   name = "test-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.1.1/24"
 
   azs                  = ["us-west-1a", "us-west-1b"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets       = ["10.1.3.0/24", "10.1.4.0/24"]
   enable_dns_hostnames = true
 
   depends_on = ["bad_reference"]
